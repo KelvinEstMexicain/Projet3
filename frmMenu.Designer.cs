48e1076c2b6206df319e8d9ccb00fa9db1f71112
KelvinEstMexicain <d.lusignan@cgodin.qc.ca>
2013-12-02 15:42:41 -0500

    Mise à jour de frmMenu. Ajout de bouton.

--- frmMenu.Designer.cs
+++ frmMenu.Designer.cs
@@ -32,6 +32,10 @@
             this.btnGestionEmployes = new System.Windows.Forms.Button();
             this.btnAjoutDepenses = new System.Windows.Forms.Button();
             this.btnReabonnement = new System.Windows.Forms.Button();
+            this.btnIncrirePartie = new System.Windows.Forms.Button();
+            this.btnInscrireDepense = new System.Windows.Forms.Button();
+            this.btnRapport = new System.Windows.Forms.Button();
+            this.btnStatistiques = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAjoutAbonnement
@@ -78,11 +82,55 @@
             this.btnReabonnement.UseVisualStyleBackColor = true;
             this.btnReabonnement.Click += new System.EventHandler(this.btnReabonnement_Click);
             // 
+            // btnIncrirePartie
+            // 
+            this.btnIncrirePartie.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIncrirePartie.Location = new System.Drawing.Point(159, 113);
+            this.btnIncrirePartie.Name = "btnIncrirePartie";
+            this.btnIncrirePartie.Size = new System.Drawing.Size(122, 84);
+            this.btnIncrirePartie.TabIndex = 6;
+            this.btnIncrirePartie.Text = "Inscrire une partie de golf jouée";
+            this.btnIncrirePartie.UseVisualStyleBackColor = true;
+            // 
+            // btnInscrireDepense
+            // 
+            this.btnInscrireDepense.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscrireDepense.Location = new System.Drawing.Point(159, 219);
+            this.btnInscrireDepense.Name = "btnInscrireDepense";
+            this.btnInscrireDepense.Size = new System.Drawing.Size(122, 60);
+            this.btnInscrireDepense.TabIndex = 7;
+            this.btnInscrireDepense.Text = "Inscrire des dépenses";
+            this.btnInscrireDepense.UseVisualStyleBackColor = true;
+            // 
+            // btnRapport
+            // 
+            this.btnRapport.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRapport.Location = new System.Drawing.Point(12, 315);
+            this.btnRapport.Name = "btnRapport";
+            this.btnRapport.Size = new System.Drawing.Size(122, 60);
+            this.btnRapport.TabIndex = 8;
+            this.btnRapport.Text = "Visualiser les rapports";
+            this.btnRapport.UseVisualStyleBackColor = true;
+            // 
+            // btnStatistiques
+            // 
+            this.btnStatistiques.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistiques.Location = new System.Drawing.Point(159, 315);
+            this.btnStatistiques.Name = "btnStatistiques";
+            this.btnStatistiques.Size = new System.Drawing.Size(122, 60);
+            this.btnStatistiques.TabIndex = 9;
+            this.btnStatistiques.Text = "Visualiser les statistiques";
+            this.btnStatistiques.UseVisualStyleBackColor = true;
+            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(352, 387);
+            this.Controls.Add(this.btnStatistiques);
+            this.Controls.Add(this.btnRapport);
+            this.Controls.Add(this.btnInscrireDepense);
+            this.Controls.Add(this.btnIncrirePartie);
             this.Controls.Add(this.btnReabonnement);
             this.Controls.Add(this.btnAjoutDepenses);
             this.Controls.Add(this.btnGestionEmployes);
@@ -99,5 +147,9 @@
         private System.Windows.Forms.Button btnGestionEmployes;
         private System.Windows.Forms.Button btnAjoutDepenses;
         private System.Windows.Forms.Button btnReabonnement;
+        private System.Windows.Forms.Button btnIncrirePartie;
+        private System.Windows.Forms.Button btnInscrireDepense;
+        private System.Windows.Forms.Button btnRapport;
+        private System.Windows.Forms.Button btnStatistiques;
     }
 }
